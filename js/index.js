a6cd8c7e5b02f7d1a7066f988f9f5b5189762dea
Olamide1992 <mikaheelazeez192@gmail.com>
2022-11-17 18:20:53 -0800

    rect

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
-// let cartIcon = document.querySelector("#cart-icon");
-// let cart = document.querySelector(".cart");
+ // Cart
+let cartIcon = document.querySelector("#cart-icon");
+let cartdetails = document.querySelector(".cart");
 
-document.getElementById("cart-icon").addEventListener("click", cartIcon);
-
-// // Open Cart
-function cartIcon() {
-  document.querySelectorAll(".cart").innerHTML = "cart"
+///open cart
+cartIcon.onclick = () =>{
+  cartdetails.classList.add("active");
 }
 
 
 
+// document.getElementById("cart-icon").addEventListener("click", cartIcon);
+ 
+// // // Open Cart
+// function cartIcon() {
+//   document.querySelectorAll(".cart").innerHTML = "cart"
+// }
 
+// function cartIcon() {
+//    var cartSelection =document.getElementById('cart-info');
+//    cartSelection.o
+// }
+
+// var thecart = document.getElementById("cart-icon");
+
+ 
+// https://righteous08.github.io/Carreer/
 // document.getElementsByClassName(".g")
 // cartIcon.onclick = () => {
 //   cart.classList.add("active");
